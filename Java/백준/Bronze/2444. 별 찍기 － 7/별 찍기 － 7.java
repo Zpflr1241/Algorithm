1e8d51d2362b510c43454a03002cb634d9b807b5
Zpflr1241 <165247416+Zpflr1241@users.noreply.github.com>
2025-04-01 15:07:19 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 384 ms, Memory: 22964 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		for(int i=1; i<=num; i++) {
+			for(int j = 0; j<num-i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j<(i*2-1); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=num-1; i>=1; i--) {
+			for(int j = 0; j<num-i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j<(i*2-1); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
