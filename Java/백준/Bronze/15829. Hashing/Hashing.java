d497d6ce41c34c43c364633650bd642c6ca89e10
Zpflr1241 <165247416+Zpflr1241@users.noreply.github.com>
2025-04-08 13:49:18 +0900

    [Bronze II] Title: Hashing, Time: 172 ms, Memory: 17796 KB, Score: 100 point -BaekjoonHub

--- Java/백준/Bronze/15829. Hashing/Hashing.java
+++ Java/백준/Bronze/15829. Hashing/Hashing.java
@@ -1,18 +1,22 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int L = Integer.parseInt(sc.nextLine());
-		int res = 0;
-		String arrs[] = sc.nextLine().split("");
-		for(int i = 0; i < L; i++) {
-			res += ((arrs[i].charAt(0))-96)*Math.pow(31,i);
+		BigInteger res = new BigInteger(0+"");
+		BigInteger val = new BigInteger(1+"");
+		String arr = sc.nextLine();
+		long mod = 1234567891;
+		for(int i = 0; i < arr.length(); i++) {
+			res = res.add(val.multiply(BigInteger.valueOf(arr.charAt(i)-96)));
+			res = res.mod(BigInteger.valueOf(mod));
+			val = val.multiply(BigInteger.valueOf(31)).mod(BigInteger.valueOf(mod));
 		}
 		System.out.println(res);
 		sc.close();
-	}
 
+	}
  }
