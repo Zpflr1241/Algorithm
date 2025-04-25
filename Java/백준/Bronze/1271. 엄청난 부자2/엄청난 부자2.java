699bb200cb0eb94baa2a324ea2984a88a8bd1bf3
Zpflr1241 <165247416+Zpflr1241@users.noreply.github.com>
2025-04-25 23:34:20 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 188 ms, Memory: 17716 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringTokenizer st = new StringTokenizer(sc.nextLine());
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
+
+        System.out.println(n.divide(m));
+        System.out.println(n.mod(m));
+    }
+}
