3a1330eeb04ab7bbe9c9e1f2c88595ed447e33b4
Zpflr1241 <165247416+Zpflr1241@users.noreply.github.com>
2025-07-05 16:25:05 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 276 ms, Memory: 20780 KB -BaekjoonHub

--- Java/백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.java
+++ Java/백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.java
@@ -3,17 +3,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] A = sc.next().split("");
-        if(A[0].equals("0")) System.out.println(0);
-        else {
-            String[] B = sc.next().split("");
-            long res = 0;
-            for (String a : A) {
-                for (String b : B) {
-                    res += (long) Integer.parseInt(a) * Integer.parseInt(b);
-                }
-            }
-            System.out.println(res);
+        int[] A;
+        int[] B;
+        String input1 = sc.next();
+        if(input1.equals("0")){
+            System.out.println(0);
+            return;
         }
+        String input2 = sc.next();
+        A = new int[input1.length()];
+        B = new int[input2.length()];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = Integer.parseInt(input1.charAt(i)+"");
+        }
+        for (int i = 0; i < B.length; i++) {
+            B[i] = Integer.parseInt(input2.charAt(i)+"");
+        }
+        long res = 0;
+        for (int a : A) {
+            for (int b : B) {
+                res += (long) a*b;
+            }
+        }
+        System.out.println(res);
+
     }
 }
